b06bfe2404dc7e128e81a9b7a6bf7b20352ccfbc
Sai-Chintala <sai.chintala413@gmail.com>
2022-04-10 16:19:34 +0530

    flask url with error handling to work in Post-Office

---
+++ main8.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def base_route():
+    return "Welcome to Praxis"
+
+@app.route('/my_name/<name>')
+def print_name(name):
+    return f"Happy learning {name} in Praxis Bangalore"
+
+@app.route('/hello',methods = ["GET", "POST"])
+def hello():
+    try:
+        stud_name = request.args.get("StudentName")
+        numb = request.args.get("RollNo")
+        
+        return f"Student Name is {stud_name} and Roll Number is {numb}", 200
+
+    except Exception as e:
+        return f"Error occured with message : {e}", 401
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0",port = 8000, debug = True)
