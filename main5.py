c80bf8d41771a66b88f801b82c75d0c9d404d929
Sai-Chintala <sai.chintala413@gmail.com>
2022-04-10 14:41:50 +0530

    flask worked with dynamic url

---
+++ main5.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def base_route():
+    return "Welcome to Praxis"
+
+@app.route('/my_name/<name>')
+def print_name(name):
+    return f"Happy learning {name} in Praxis Bangalore"
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0",port = 8000)
